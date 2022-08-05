4574296ec37ad59a595d231038e53d6c908caa2c
sanjay7900 <101556258+sanjay7900@users.noreply.github.com>
2022-08-05 09:55:24 +0530

    Delete Assessment3_04_08_BoolSale directory

--- Assessment3_04_08_BoolSale/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Assessment3_04_08_BoolSale.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
