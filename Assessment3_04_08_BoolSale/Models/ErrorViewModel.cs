3e7894f3e4bdf805a2e58292fd0664c62343a0c4
sanjay7900 <singhsanjay790043@gmail.com>
2022-08-05 17:19:27 +0530

    initial commit

---
+++ Assessment3_04_08_BoolSale/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assessment3_04_08_BoolSale.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
